67ee43222410d9d7e8e2fdffc13bc4a949c51b54
desarrollador55 <al02841772@tecmilenio.mx>
2023-03-22 22:08:39 -0700

    commit

---
+++ routes/web.php
@@ -0,0 +1,28 @@
+<?php
+use App\Http\Controllers\HomeController;
+use Illuminate\Support\Facades\Route;
+use App\Mail\WelcomeMail;
+use Illuminate\Support\Facades\Mail;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes();
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::get('/email', function () {
+    //Mail::to('ejemplo@gmail.com')->send(new WelcomeMail());
+    return new WelcomeMail();
+});
